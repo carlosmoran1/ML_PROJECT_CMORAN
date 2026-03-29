9873144a7f0f3c914502db43b164ff1008b42c4c
carlosmoran1 <csaraviam@estudiante.uc.cl>
2026-03-29 19:18:09 -0300

    corrección main en etl

--- src/pipelines/etl_market_data/main.py
+++ src/pipelines/etl_market_data/main.py
@@ -1,3 +1,4 @@
+import os
 import warnings
 
 import functions_framework
@@ -11,24 +12,15 @@ except ImportError:
 
 warnings.filterwarnings("ignore")
 
-
-# ==========================================
-# CONFIGURACIÓN
-# ==========================================
-PROJECT_ID = "proyectos-cmoran-489000"
-BUCKET_NAME = "proyecto_commodities"
-DESTINATION_BLOB = "data/raw/data_historica_full.parquet"
-FRED_SECRET_ID = "fred-api-key"
-EIA_SECRET_ID = "eia-api-key"
+PROJECT_ID = os.getenv("PROJECT_ID", "proyectos-cmoran-489000")
+BUCKET_NAME = os.getenv("BUCKET_RAW", "proyecto_commodities")
+DESTINATION_BLOB = os.getenv("DESTINATION_BLOB", "data/raw/data_historica_full.parquet")
+FRED_SECRET_ID = os.getenv("FRED_SECRET_ID", "fred-api-key")
+EIA_SECRET_ID = os.getenv("EIA_SECRET_ID", "eia-api-key")
 
 
 @functions_framework.http
 def etl_commodities(request):
-    """
-    Endpoint HTTPS:
-    - Ejecuta run_etl()
-    - Devuelve 200 si OK, 500 si falla
-    """
     try:
         fred_api_key = get_secret(PROJECT_ID, FRED_SECRET_ID)
         eia_api_key = get_secret(PROJECT_ID, EIA_SECRET_ID)
@@ -44,16 +36,3 @@ def etl_commodities(request):
     except Exception as e:
         print(f"ERROR FATAL en ETL: {e}")
         return (f"ERROR: {e}", 500)
-
-
-if __name__ == "__main__":
-    fred_api_key = get_secret(PROJECT_ID, FRED_SECRET_ID)
-    eia_api_key = get_secret(PROJECT_ID, EIA_SECRET_ID)
-
-    run_etl(
-        project_id=PROJECT_ID,
-        bucket_name=BUCKET_NAME,
-        destination_blob=DESTINATION_BLOB,
-        fred_api_key=fred_api_key,
-        eia_api_key=eia_api_key,
-    )
