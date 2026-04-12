1a4934a5b92ce2ddc8f6d0f4df010c3741eb62ee
carlosmoran1 <csaraviam@estudiante.uc.cl>
2026-04-12 00:46:03 -0400

    actualización de readme e integración data dimension sql

---
+++ SQL/dimensiones/tables/dim_descripcion_variables_model.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `proyectos-cmoran-489000.dimensiones_parametricas.dim_descripcion_variables_model`
+(
+  commodity STRING,
+  commodity_nombre STRING,
+  frecuencia_prediccion STRING,
+  dias_sin_fds INT64,
+  variable STRING,
+  orden_variable INT64,
+  variable_larga STRING,
+  descripcion STRING
+);
